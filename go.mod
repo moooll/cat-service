01587587ff288d30a352216ae503bc5519c8c577
Marya Chaprasava <mariacheprasova25@gmail.com>
2021-08-03 19:06:19 +0300

    feat: added replaced map storage with psql, \ without update enpoint and database function

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
-module cat-crud
+module cat-service
 
 go 1.16
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/jackc/pgx/v4 v4.13.0
 	github.com/labstack/echo/v4 v4.5.0
+	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7
 	go.uber.org/zap v1.18.1
 )
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"cat-crud/storage"
-	
+	"cat-service/db/psql"
+	"context"
+	"os"
+
 	"log"
 
+	"github.com/jackc/pgx/v4"
 	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
 )
@@ -16,19 +19,24 @@ func main() {
 	}
 
 	defer logger.Sync()
-	// init storage
-	catStore := storage.NewCatalog()
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		zap.L().Error("could not connect to the psql")
+		os.Exit(1)
+	}
+
+	defer conn.Close(context.Background())
+
+	catalog := psql.NewCatalog(conn)
 	service := &Service{
-		*catStore,
+		catalog,
 	}
 	e := echo.New()
-
 	e.POST("/cats/add", service.addCat)
 	e.GET("/cats", service.getAllCats)
 	e.GET("/cats/:id", service.getCat)
-	e.GET("/cats/:name", service.getCatName)
-	e.PUT("/cats/:id", service.updateCat)
-	e.PUT("/cats/:name", service.updateCatName)
+	// e.PUT("/cats/:id", service.updateCat)
 	e.DELETE("/cats/:id", service.deleteCat)
 
 	if err := e.Start(":8081"); err != nil {
